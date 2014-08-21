f0add400adcf5c25b999d7a27cc33d707820dc4b
Eli McMakin <elimc184@outlook.com>
2014-08-21 10:01:30 -0500

    Added gulp.js settings.

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+// Sources
+var sassWatch = './lib/scss/**/*.scss',
+    sassSource = './lib/scss/*.scss',
+    sassDestination = './style.scss',
+    phpWatch = './**/*.php',
+    css = './style.scss';
+
+// Set the proxy.
+gulp.task('browser-sync', function () {
+    browserSync({
+        proxy: "localhost/jump_start/"
+    });
+});
+
+// Compile Sass file to CSS, and reload browser(s).
+gulp.task('sass', function() {
+    return gulp.src(sassSource)
+        .pipe(sass())
+        .pipe(gulp.dest(sassDestination))
+        .pipe(reload({stream:true}));
+});
+
+// Reload Index
+gulp.task('php-reload', function() {
+    return gulp.src(phpWatch)
+        .pipe(reload({stream:true}));
+});
+
+// Reload CSS
+gulp.task('css-reload', function() {
+    return gulp.src(css)
+        .pipe(reload({stream:true}));
+});
+
+// Run a series of tasks when "gulp" is entered in the CLI.
+gulp.task('default', ['sass','browser-sync'], function() {
+    gulp.watch(sassWatch, ['sass']);
+    gulp.watch(phpWatch, ['php-reload']);
+    gulp.watch(css, ['css-reload']);
+});
