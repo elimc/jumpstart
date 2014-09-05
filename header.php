ea763bb6daa390a2093fa9d37584087edd1e8a7d
Eli McMakin <elimc184@outlook.com>
2014-09-05 02:06:49 -0500

    Added a favicon link.

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
 
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+
 <?php wp_head(); ?>
 </head>
 
