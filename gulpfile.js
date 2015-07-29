90494bd6f1c243106a7b4b3a092924ca9c793653
Eli McMakin <elimc184@outlook.com>
2015-07-29 12:05:51 -0500

    Foundation components now loaded and minified in gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -61,10 +61,32 @@ gulp.task('sass', function() {
 // Concatenates Foundation's JS, for fewer HTTP requests, and spits out code to browser.
 gulp.task('foundation', function() {
     return gulp.src([
+        
+        // Foundation core - needed if you want to use any of the components below
         './bower_components/foundation/js/foundation/foundation.js',
         './bower_components/foundation/js/vendor/fastclick.js',
         './bower_components/foundation/js/vendor/placeholder.js',
         './bower_components/foundation/js/vendor/modernizr.js',
+
+        // Pick the componenets you need in your project
+        //'./bower_components/foundation/js/foundation/foundation.abide.js',
+        //'./bower_components/foundation/js/foundation/foundation.accordion.js',
+        //'./bower_components/foundation/js/foundation/foundation.alert.js',
+        //'./bower_components/foundation/js/foundation/foundation.clearing.js',
+        //'./bower_components/foundation/js/foundation/foundation.dropdown.js',
+        //'./bower_components/foundation/js/foundation/foundation.equalizer.js',
+        //'./bower_components/foundation/js/foundation/foundation.interchange.js',
+        //'./bower_components/foundation/js/foundation/foundation.joyride.js',
+        //'./bower_components/foundation/js/foundation/foundation.magellan.js',
+        //'./bower_components/foundation/js/foundation/foundation.offcanvas.js',
+        //'./bower_components/foundation/js/foundation/foundation.orbit.js',
+        //'./bower_components/foundation/js/foundation/foundation.reveal.js',
+        //'./bower_components/foundation/js/foundation/foundation.slider.js',
+        //'./bower_components/foundation/js/foundation/foundation.tab.js',
+        //'./bower_components/foundation/js/foundation/foundation.tooltip.js',
+        //'./bower_components/foundation/js/foundation/foundation.topbar.js', 
+          		  
+        // Initializes Foundation JS.
         './lib/js/vendor/foundation-bootstrap.js'
     ])
         .pipe(concat('foundation.min.js'))
--- lib/inc/enque.php
+++ lib/inc/enque.php
@@ -6,36 +6,13 @@
  * @link http://codex.wordpress.org/Function_Reference/wp_enqueue_script Enque scripts.
  */
 function jumpstart_scripts() {
-    // Load stylesheed with dashicons as a dependency.
-    // Learn about dashicons: http://melchoyce.github.io/dashicons/
+    // Load stylesheed with dashicons as a dependency: http://melchoyce.github.io/dashicons/
 	wp_enqueue_style( 'jumpstart-style', get_stylesheet_uri(), array( 'dashicons' ) );
 
-    // Load Base Foundation JS.
-//    wp_enqueue_script( 'foundation', FOUNDATION . '/foundation/foundation.js', array( 'jquery' ), '1.0', TRUE );
-//    wp_enqueue_script( 'fastClick', FOUNDATION . '/vendor/fastclick.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'placeholder', FOUNDATION . '/vendor/placeholder.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'modernizr', FOUNDATION . '/vendor/modernizr.js', array( 'jquery', 'foundation' ), '1.0', FALSE );
+    // Load minified Foundation.
     wp_enqueue_script( 'foundation', LIB . '/js/min/foundation.min.js', array( 'jquery' ), '1.0', TRUE );
 
-    // Foundation Modules
-//    wp_enqueue_script( 'foundation-abide', FOUNDATION . '/foundation/foundation.abide.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-accordion', FOUNDATION . '/foundation/foundation.accordion.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-alert', FOUNDATION . '/foundation/foundation.alert.js', array( 'jquery' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-clearing', FOUNDATION . '/foundation/foundation.clearing.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-dropdown', FOUNDATION . '/foundation/foundation.dropdown.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-equalizer', FOUNDATION . '/foundation/foundation.equalizer.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-interchange', FOUNDATION . '/foundation/foundation.interchange.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-joyride', FOUNDATION . '/foundation/foundation.joyride.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-magellan', FOUNDATION . '/foundation/foundation.magellan.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-offcanvas', FOUNDATION . '/foundation/foundation.offcanvas.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-orbit', FOUNDATION . '/foundation/foundation.orbit.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-reveal', FOUNDATION . '/foundation/foundation.reveal.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-slider', FOUNDATION . '/foundation/foundation.slider.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-tab', FOUNDATION . '/foundation/foundation.tab.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-tooltip', FOUNDATION . '/foundation/foundation.tooltip.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-//    wp_enqueue_script( 'foundation-toolbar', FOUNDATION . '/foundation/foundation.topbar.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
-
-    // All custom scripts in lib/js
+    // Load minified cusom JS.
     wp_enqueue_script( 'custom-scripts', LIB . '/js/min/scripts.min.js', array( 'jquery', 'foundation' ), '1.0', TRUE );
 }
 add_action( 'wp_enqueue_scripts', 'jumpstart_scripts' );
