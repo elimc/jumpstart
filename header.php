11cede2b73432c7cf9d39d855c2bfca5587a7bb5
Eli McMakin <elimc184@outlook.com>
2015-08-09 04:30:35 -0500

    Makes IE meta tag lowercase for more consistency in header.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
