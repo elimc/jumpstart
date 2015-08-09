43ba3de096cc32f2da6b103188f5a04dd5ba4c93
Eli McMakin <elimc184@outlook.com>
2015-08-09 04:09:16 -0500

    Adds base sass page and adds a friendly instruction for IE8 users to upgrade.

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
 </head>
 
 <body>
+    <!--[if lt IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
 	<header>
 		<div>
 			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
