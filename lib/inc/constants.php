5e6b6a7bd0e8cc1ce3851814c014794b9200afb6
Eli McMakin <elimc184@outlook.com>
2015-08-14 20:22:55 -0500

    Deletes vestigial constant.

--- lib/inc/constants.php
+++ lib/inc/constants.php
@@ -13,10 +13,5 @@ if ( !defined( 'AUTHOR' ) ) { define('AUTHOR', 'Eli McMakin'); }
 
 if ( !defined( 'LIB' ) ) {
     $lib = get_stylesheet_directory_uri() . "/lib";
-    define('LIB', $lib );
-}
-
-if ( !defined( 'FOUNDATION' ) ) {
-    $lib = get_stylesheet_directory_uri() . "/bower_components/foundation/js";
-    define('FOUNDATION', $lib );
+    define( 'LIB', $lib );
 }
